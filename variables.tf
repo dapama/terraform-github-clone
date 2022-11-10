2e7cc6e639696350d4f4e35ab31de6368292ff49
DavidPM <davidpm@davidpm-Executive>
2022-11-10 11:05:07 +0100

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "repository_name" {
+  type        = string
+  description = "Name of the repository to retrieve the release from."
+}
+
+variable "tag" {
+  type        = string
+  description = "Repository tag."
+}
