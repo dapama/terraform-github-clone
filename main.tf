996acd12a943b9fb2dae75c77064fc7683fd9fe9
DavidPM <davidpm@davidpm-Executive>
2022-11-10 11:25:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 
 resource "null_resource" "git_clone" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
-    command = "../scripts/git_clone.sh -t 300"
-    interpreter = ["bash"]
+    command = "./scripts/git_clone.sh -t 300"
 
     environment = {
       REPOSITORY_NAME = var.repository_name
